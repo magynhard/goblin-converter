87e082560d10b54dff19a6ec599c06a817e7eca3
magynhard <github@mail.magynhard.de>
2025-01-04 23:25:03 +0100

    refactor install script

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
+<img src="res/logo.png" style="height: 128px; float: left;">
+
 # Goblin document converter
 [![Ruby](https://img.shields.io/badge/_-Ruby-Sub?style=plastic&color=gray&logo=ruby&logoColor=red)](#)
 [![GTK](https://img.shields.io/badge/_-GTK-Sub?style=plastic&color=gray&logo=gtk&logoColor=green)](#)
 [![GTK](https://img.shields.io/badge/_-magick-Sub?style=plastic&color=gray&logo=gnome-terminal&logoColor=)](#)
 [![License: MIT](https://img.shields.io/badge/License-MIT-gold.svg?style=plastic&logo=mit)](LICENSE)
 
+<br>
+
+> 
 > A simple document converter GUI for GTK using magick
 
 Initial use case was to convert 300ppi (PDF) documents (grayscale or colored) to monochrome (PDF) documents to reduce the size significantly for digital document storage.
--- install.sh
+++ install.sh
@@ -1,1 +1,33 @@
 #!/usr/bin/env bash
+
+# Check if ruby command is available
+if ! command -v ruby &> /dev/null; then
+  echo "Error: Ruby is not installed. Please install Ruby to continue."
+  exit 1
+fi
+
+# Check if bundle command is available
+if ! command -v bundle &> /dev/null; then
+  echo "Error: Bundler is not installed. Please install Bundler by running 'gem install bundler'."
+  exit 1
+fi
+
+# Get the current Ruby version
+RUBY_VERSION=$(ruby -v | awk '{print $2}')
+
+# Create .ruby-version file with the current Ruby version
+echo "$RUBY_VERSION" > .ruby-version
+
+# Get the current path
+CURRENT_PATH=$(pwd)
+
+# Run bundle install to install dependencies
+bash -c "bundle install"
+
+# Target path for the .desktop file
+DESKTOP_FILE="$HOME/.local/share/applications/goblin-document-converter.desktop"
+
+# Copy the .desktop file and replace placeholders
+sed "s|{{path_to_goblin_document_converter_root}}|$CURRENT_PATH|g" res/goblin-document-converter.desktop > "$DESKTOP_FILE"
+
+echo "Installation complete. You can now run Goblin Document Converter from the applications menu."
